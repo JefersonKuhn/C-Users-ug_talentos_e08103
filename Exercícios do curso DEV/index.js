9cfb96f64a1053bf682f8a5ab4b2e11a52ccac14
JefersonKuhn <166512818+JefersonKuhn@users.noreply.github.com>
2024-04-24 18:03:58 -0300

    Add files via upload

---
+++ Exercícios do curso DEV/index.js
@@ -0,0 +1,101 @@
+// EXERCÍCIOS
+
+/*console. log("hello world")
+const nome = "Jeferson Kuhn"
+const idade = 36
+console.log ("Meu nome é" ,nome,"e tenho",idade, "anos de idade")
+
+// teste
+const idadeNumero = 23
+const idadetexto = idadeNumero.toString()
+console.log(typeof idadeNumero)
+console.log(typeof idadeTexto)
+CustomElementRegistry
+prompt ("Qual é o seu nome")
+console.log ("Qual é o seu nome")
+
+// teste2
+console.log("Qual é o seu nome")
+console.log("Jeferson Kuhn")
+prompt("Jeferson Kuhn")
+const idadeNumero = 36
+const idadetexto = idadetexto.toString()
+<script src="./index.js" defer ></script>*/
+
+// teste3
+/*const primeironome = "jeferson"
+const segundonome = "kuhn"
+const idadenumero = "36"
+const serestudante = "sim"
+console.log("o seu nome é",primeironome,"o seu sobrenome",segundonome,"minha idade",idadenumero,"é estudante",serestudante,)
+let nome = prompt ("Qual é o seu nome?")
+alert (`Seu nome é ${nome}`)*/
+
+
+// EXERCÍCIOS
+
+/*// recebeu as variáveis
+let nome = prompt("Digite seu nome")
+let idade = prompt("Digite sua idade")
+
+// mensagem: Digite as horas que você trabalha por dia:
+let horastrabalho = prompt("Digite as horas que você trabalha pro dia")
+let salarioDia = prompt("Digite seu salário diário:")
+
+// Verificando as variáveis 
+console.log(typeof nome)
+console.log(typeof horastrabalho)
+console.log(typeof salarioDia)
+
+//Convertendo o que é necessário 
+let horastraNumber = Number(horastrabalho)
+let salDiaNumber = Number(salarioDia)
+
+//processando valores
+let valHoraNumber = salDiaNumber/horastraNumber
+
+// conversão para não dar erro de Null
+let valorHora = string(valHoraNumber)
+const mensagem = "seu valor hora e" + valorHora + "!!!"*/
+
+//EXERCÍCIOS
+
+/*1. Faça um programa que:
+    
+    a) Pergunte a idade do usuário
+    
+    b) Pergunte a idade do seu melhor amigo ou da sua melhor amiga
+    
+    c) **Imprima no console** a seguinte mensagem: "Sua idade é maior do que a do seu melhor amigo?", 
+    seguido pela resposta (`true` ou `false`)
+    
+    d) **Imprima no console** a diferença de idade (não tem problema se sair um número negativo)*/
+
+   /* let idade  = prompt("Digite sua idade")
+    let idadeAmigo  = prompt("Digite a idade de seu amigo")
+    const mensagem = "Sua idade é"
+    //resultado = idade === idadeAmigo
+    //alert(resultado)
+    
+    resultado = idade !== idadeAmigo
+    alert(`A idade do amigo é diferenteS? ${resultado}`)
+    
+    resultado = idade >= idadeAmigo
+    alert(`A idade é maior? ${resultado}`)
+    
+    resultado = idadeAmigo < idade
+    alert(`Idade do amigo é menor? ${resultado}`)
+    
+    diferençaidade = idade - idadeAmigo
+    alert(`diferença de idade é ${diferençaidade}`)
+
+// verificando variáveis 
+console.log (typeof valorHora)
+console.log (typeof mensagem)
+console.log (mensagem)
+
+// Saída de valores para Front End
+alert(mensagem)*/
+
+
+
