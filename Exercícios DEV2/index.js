9c1d37004eb408f903cbaad2093d741e21ab4cb1
JefersonKuhn <166512818+JefersonKuhn@users.noreply.github.com>
2024-04-24 21:32:39 -0300

    Add files via upload

---
+++ Exercícios DEV2/index.js
@@ -0,0 +1,78 @@
+/*1. Faça um programa que:
+    
+    a) Pergunte a idade do usuário
+    
+    b) Pergunte a idade do seu melhor amigo ou da sua melhor amiga
+    
+    c) **Imprima no console** a seguinte mensagem: "Sua idade é maior do que a do seu melhor amigo?", 
+    seguido pela resposta (`true` ou `false`)
+    
+    d) **Imprima no console** a diferença de idade (não tem problema se sair um número negativo)
+
+let idade  = prompt("Digite sua idade")
+let idadeAmigo  = prompt("Digite a idade de seu amigo")
+const mensagem = "Sua idade é"
+//resultado = idade === idadeAmigo
+//alert(resultado)
+
+resultado = idade !== idadeAmigo
+alert(`A idade do amigo é diferenteS? ${resultado}`)
+
+resultado = idade >= idadeAmigo
+alert(`A idade é maior? ${resultado}`)
+
+resultado = idadeAmigo < idade
+alert(`Idade do amigo é menor? ${resultado}`)
+
+diferençaidade = idade - idadeAmigo
+alert(`diferença de idade é ${diferençaidade}`)*/
+
+    
+// a) Peça ao usuário que insira um número **par**
+    
+// b) Imprima na console **o resto da divisão** desse número por 2.
+    
+//c) Teste o programa com diversos números pares. Você notou um padrão? Escreva em um comentário de código.
+
+//d) O que acontece se o usuário inserir um número ímpar? Escreva em um comentário de código
+
+/*let Numberpar  = prompt("Digite um número PAR")
+
+resultado = idade !== idadeAmigo
+alert(`A idade do amigo é diferenteS? ${resultado}`)
+
+
+3. Faça um programa que pergunte ao usuário sua idade em anos. Depois, imprima no console 
+    
+    a) A idade do usuário em meses
+    
+    b) A idade do usuário em dias
+    
+    c) A idade do usuário em horas
+
+Faça um programa que pergunte ao usuário dois números.
+ Em seguida, faça as operações e imprima no console as
+ seguintes mensagens seguidas pelo true ou false:*/
+
+
+ // Exercício de strings e comcatenar
+
+ /*let nome = prompt("Digite um número")
+ let cor = prompt("Digite sua cor favorita")
+ alert (" A cor favorita de" + nome + " é " + cor)
+alert = (`A cor favorita de ${nome} é ${cor}`)*/
+
+// Exercício alteração de frases 
+// peça para o usuário escrever uma frase e imprima no console a frase alterada, com:
+// * Todas as letras maiúsculas 
+// * Na lingua do i ( substituindo a vogal "o" por "i")
+// * O tamanho da frase 
+
+//const frase = prompt("Digite uma frase")
+//const fraseMaiuscula = frase.toUpperCase()
+
+/*let numero = prompt("Digite um número de 0 a 5")
+const listaderaças = ["Borde Collie", "Poodle", "Pastor alemão","Golden retriever","Doberman"]
+const segundoItem = listaderaças[2]
+alert (listaderaças[ numero ])*/
+
